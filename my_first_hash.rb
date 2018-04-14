caab55f916cd11cb0056e3ce040a189df0bf3943
Dani Lyn <dlyn1110@gmail.com>
2018-04-14 12:32:36 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,4 +1,9 @@
 def my_hash
+  my_hash => {
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3
+  }
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
 
