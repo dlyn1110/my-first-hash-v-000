50a415012752a7202723c689cac24a9015cc3ca7
Dani Lyn <dlyn1110@gmail.com>
2018-04-14 12:51:31 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,25 +1,25 @@
 def my_hash
   my_hash = {
-    "whale bone corsets",: 5,
-    "porcelain vases",: 2,
-    "oil paintings": 3
+    "whale bone corsets" =>  5,
+    "porcelain vases" =>  2,
+    "oil paintings" =>  3
   }
 end
 
 def shipping_manifest
   the_manifest = {
-    "whale bone corsets",: 5,
-    "porcelain vases",: 2,
-    "oil paintings": 3
+    "whale bone corsets", => 5,
+    "porcelain vases", =>  2,
+    "oil paintings" =>  3
   }
 
 end
 
 def retrieval
   shipping_manifest = {
-                  "whale bone corsets",: 5,
-                  "porcelain vases",: 2,
-                  "oil paintings": 3
+                  "whale bone corsets" =>  5,
+                  "porcelain vases" =>  2,
+                  "oil paintings" =>  3
                  }
 
 shipping_manifest["oil paintings"]
@@ -28,8 +28,8 @@ end
 
 def adding
   shipping_manifest = {
-                  "whale bone corsets", => 5,
-                  "porcelain vases", => 2,
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
   shipping_manifest["muskets"] = 2
